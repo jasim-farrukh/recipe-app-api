67a4ed472030a177e3d9b7eb7d6daa917fcca0ea
jasim321 <jasimfarrukh61@gmail.com>
2022-12-05 02:37:47 +0500

    testing the custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,35 +1,35 @@
 """
-Database models
+Database models.
 """
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin
+    PermissionsMixin,
 )
 
 
 class UserManager(BaseUserManager):
-    """Manager for Users."""
+    """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
-        """Create, save and return a new user"""
+        """Create, save and return a new user."""
         if not email:
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-        return user # returning the user that is just created
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password)
-        user.is_superuser = True
-        user.is_staff = True
-        user.save(using=self._db)
         return user
 
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
-
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -37,6 +37,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
     objects = UserManager()
+
     USERNAME_FIELD = 'email'
